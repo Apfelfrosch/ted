dd10a5cdc7edd92e843656928b27f0b8ff56536c
Robin Füllgraf <realapfelfrosch@gmail.com>
2023-12-10 21:35:09 +0100

    Display line numbers

--- src/frontend/window.rs
+++ src/frontend/window.rs
@@ -1,3 +1,8 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 use ratatui::{
     layout::Rect,
     text::Line,
@@ -16,16 +21,37 @@ pub struct Window {
     pub cursor_char_index: usize,
 }
 
+fn visual_length_of_number(i: usize) -> u32 {
+    if i == 0 {
+        1
+    } else {
+        i.ilog10() + 1
+    }
+}
+
 impl Window {
     pub fn render(&self, terminal: &mut Frame<'_>, layout_rect: Rect) {
+        let max_lines = visual_length_of_number(self.e.text.len_lines());
+
         let v = self
             .e
             .text
             .lines_at(self.scroll_y)
+            .enumerate()
             .take(layout_rect.height as usize)
-            .fold(Vec::new(), |mut acc, element| {
+            .fold(Vec::new(), |mut acc, (idx, element)| {
+                let mut line_buf = String::with_capacity(max_lines as usize + 1);
+
+                let idx = idx + 1;
+                for _ in 0..(max_lines - visual_length_of_number(idx) as u32) {
+                    line_buf.push('0');
+                }
+
+                line_buf.push_str(&idx.to_string());
+                line_buf.push(' ');
+
                 if self.scroll_x >= element.len_chars() {
-                    acc.push(Line::from(""));
+                    acc.push(Line::from(line_buf));
                     return acc;
                 }
 
@@ -38,7 +64,11 @@ impl Window {
                                     line_string.pop();
                                 }
                 */
-                acc.push(Line::from(line_string.replace('\n', "␊")));
+                acc.push(Line::from(format!(
+                    "{}{}",
+                    line_buf,
+                    line_string.replace('\n', "␊")
+                )));
                 acc
             });
         terminal.render_widget(
@@ -63,7 +93,7 @@ impl Window {
 
         let current_line_start = self.e.text.line_to_char(current_line);
         let cursor_y = current_line - self.scroll_y + 1;
-        let mut cursor_x = 1;
+        let mut cursor_x = (1 + visual_length_of_number(self.e.text.len_lines()) + 1) as usize;
         for i in 0..(self.cursor_char_index - current_line_start) {
             cursor_x +=
                 unicode_width::UnicodeWidthChar::width(self.e.text.char(current_line_start + i))
@@ -78,7 +108,8 @@ impl Default for Window {
         Window {
             ident: "Window".to_string(),
             e: Editor {
-                text: Rope::from_str("SOME TEXT\nLine 2\nLine 3"),
+                text: Rope::from_reader(BufReader::new(File::open("test_text.txt").unwrap()))
+                    .unwrap(),
             },
             scroll_x: 0,
             scroll_y: 0,
